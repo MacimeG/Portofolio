c501dd49fe5930117a7aa34a9e99b80d7c617565
MacimeG <maxime.glaneux@gmail.com>
2021-12-13 16:26:50 +0100

    typeWriterJS

--- animate.js
+++ animate.js
@@ -1,1 +1,21 @@
 const NOM = document.querySelector('#Nom');
+
+
+
+const typewriter = new Typewriter(NOM, {
+  loop: false,
+  delay: 75,
+});
+
+typewriter
+  .changeDelay(50)
+  .pauseFor(2500)
+  .typeString('Glaneux Maxime <br>')
+  .pauseFor(20)
+  .typeString('Développeur web')
+  .pauseFor(10)
+  .deleteChars(2)
+  .typeString('eb et web <strong>mobile</strong> <br>')
+  .pauseFor(30)
+  .typeString("Front-End/Back-End")
+  .start();
